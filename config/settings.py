b2ab414c86daf40e088bae46addaa9ef3f5dd31c
b-hyoung <87049249+b-hyoung@users.noreply.github.com>
2026-03-17 17:32:33 +0800

    #우진파일 (#6)
    * Create 0001_initial.py
    
    * 계획/목표 오늘의 투두 리스트 캘린더
    
    * Yhk (#2)
    
    * 출결 간단하게 완성
    
    추후 좌석표와 연동해야됨
    
    * 중간 병합용 커밋
    
    * 중간저장 : Weeklist 투두리스트, 캘린더 (#3)
    
    * Create 0001_initial.py
    
    * 계획/목표 오늘의 투두 리스트 캘린더
    
    ---------
    
    Co-authored-by: b-hyoung <87049249+b-hyoung@users.noreply.github.com>
    
    * 추가 : 희망 직무 기반 프로필 분석 및 공고 추천 기능 (#4)
    
    * 추가 : 크롤링정보
    
    * Add student auth flow and refresh auth UI
    
    * Move Django secret key to env and simplify env example
    
    * Add job crawling detail views and list UX
    
    * 프로필 분석 기능과 공고 추천 흐름을 개선
    
    * 희망 직무 방향과 시장 분석 기반을 추가
    
    * 프로필 분석 흐름과 보완 피드백 UI를 개선
    
    * 플래너 화면 복구와 마이그레이션 병합을 반영
    
    * 중간점검 땡겨온거
    
    * 구글 캘린더랑 연동
    
    * 투두리스트 수정
    
    1. 투두리스트 일정 체크박스 왼쪽으로 이동, 아이콘 변경
    2. 투두리스트 계획 완료시 우측 하단으로 이동 기능
    
    * 캘린더 추가/수정기능 변경
    
    1. 일정 추가 후 날짜 기간입력 기능 추가
    2. 2일이상 일정은 캘린더에서 이어지게 보이도록 UI변경
    3. 캘린더 추가, 수정버튼 위치변경(수정버튼은 기능도 변경)
    
    * 캘린더 기능추가, 오류수정
    
    1. 캘린더 옆 일정에서 완료 취소를 누르면 다시 투두 리스트로 올라가는 기능 추가
    2. 캘린더 추가기능 오류 수정(투두리스트랑 캘린더 옆이랑 두개가 한꺼번에 추가되는 오류)
    3. 투두리스트에 추가 버튼 삭제
    
    * 구글 연동 기능 변경
    
    1. 구글 캘린더 연결됨 안내 위치 변경
    2. 일정 가져오기 버튼 삭제
    3. 구글 아이디 연동시 항상 연동되도록 변경
    
    * 캘린더 색깔 기능 추가
    
    1. 일정 추가/수정에 색깔 넣는 기능 추가
    2. 캘린더 미리보기에 색깔 반영
    
    ---------
    
    Co-authored-by: thdgkdud75 <dnwlsfirst75@gmail.com>
    Co-authored-by: dbgusrl2247 <yhk041018@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -134,8 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
