32cec467435a4b9741c0756a4089bc71bc25be2c
b-hyoung <youqlrqod@gmail.com>
2026-03-17 18:47:51 +0900

    fix: 마이그레이션 충돌 재수정

--- planner/migrations/0015_merge.py
+++ planner/migrations/0015_merge.py
@@ -4,7 +4,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('planner', '0011_merge_20260313_1704'),
+        ('planner', '0012_merge_20260313_1720'),
         ('planner', '0014_dailytodo_color_weeklygoal_color'),
     ]
 
