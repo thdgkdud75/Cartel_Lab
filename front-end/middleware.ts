2d4d22ef2ad4326c4d2c8aa5d2637d192ecef2c2
b-hyoung <youqlrqod@gmail.com>
2026-04-02 16:45:33 +0900

    fix: 미들웨어 matcher를 보호 경로만 지정하도록 수정 (/, /login 공개)

--- front-end/middleware.ts
+++ front-end/middleware.ts
@@ -1,25 +1,24 @@
 import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware() {
-    return NextResponse.next();
+export default withAuth({
+  pages: {
+    signIn: "/login",
   },
-  {
-    callbacks: {
-      authorized: ({ token, req }) => {
-        const { pathname } = req.nextUrl;
-        const publicPaths = ["/", "/login", "/signup"];
-        if (publicPaths.includes(pathname)) return true;
-        return !!token;
-      },
-    },
-    pages: {
-      signIn: "/login",
-    },
-  }
-);
+});
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|fonts).*)"],
+  matcher: [
+    "/attendance/:path*",
+    "/dashboard/:path*",
+    "/certifications/:path*",
+    "/contests/:path*",
+    "/blog/:path*",
+    "/users/:path*",
+    "/seats/:path*",
+    "/timetable/:path*",
+    "/quiz/:path*",
+    "/planner/:path*",
+    "/jobs/:path*",
+    "/mypage/:path*",
+  ],
 };
